5be04beb3e50cecc0db9b85bc4894ba6d15949fe
junkdog505 <junkdog505@gmail.com>
2022-01-13 16:15:56 -0500

    Se agrega al proyecto

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, Intents } = require('discord.js');
+
+require("dotenv").config()
+const TOKEN = "OTMxMjY1OTk4MjAyNDk1MDI2.YeB7DA.RnKi1gM0UZkFwddy-cZ6NoUm7NU"
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.on("ready", () => {
+    console.log(`Ingresando como ${client.user.tag}. Estoy listo!`)
+  })
+
+client.on("messageCreate", (message) =>{
+  if (message.content == "hi"){
+    message.reply("Hello world! uwu")
+  }
+})
+
+client.login(process.env.TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
